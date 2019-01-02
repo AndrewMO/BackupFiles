1e9ddffdff58be37300ff3fafc6241e3ec4dbaec
Andrew Jia <Andrew.Jia@activenetwork.com>
2019-01-02 15:30:02 +0800

    update display format

--- JenkinsRelated/StandardPool_InitOrg.bat
+++ JenkinsRelated/StandardPool_InitOrg.bat
@@ -5,15 +5,16 @@ echo "orgname=%orgname%"
 echo "########################################################################################"
 set address1="http://perf-activenet-19w.an.active.tan:3000/%orgname%/servlet/adminlogin.sdi"
 set address2="http://perf-activenet-20w.an.active.tan:3000/%orgname%/servlet/adminlogin.sdi"
-echo "address1=%address1%"
-echo "address2=%address2%"
+cd %curllocation%
 echo "########################################################################################"
 echo "---Start initialing---"
-cd %curllocation%
-
-
+echo "address1=%address1%"
+echo "----------------------------------------------------------------------------------------"
 curl -i %address1%
+echo "########################################################################################"
+echo "address2=%address2%"
+echo "----------------------------------------------------------------------------------------"
 curl -i %address2%
-
+echo "########################################################################################"
 echo "---End initialing---"
 exit
