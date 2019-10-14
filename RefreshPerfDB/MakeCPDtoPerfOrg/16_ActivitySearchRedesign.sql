534eb7866eab704b373849ad788428d10a242721
Jia <ajia@xia34541m.active.local>
2019-10-14 11:04:57 +0800

    add activity redesign switch

---
+++ RefreshPerfDB/MakeCPDtoPerfOrg/16_ActivitySearchRedesign.sql
@@ -0,0 +1,15 @@
+use acm01vegasjetty
+
+
+
+if not exists (select keywordvalue from systeminfo where KEYWORD = 'enable_new_cui_activity_search_redirect')
+BEGIN
+    insert into systeminfo (KEYWORD,KEYWORDVALUE) VALUES ('enable_new_cui_activity_search_redirect','true')
+END
+else
+BEGIN
+    update systeminfo set KEYWORDVALUE = 'true' where KEYWORD = 'enable_new_cui_activity_search_redirect'
+END
+
+
+
